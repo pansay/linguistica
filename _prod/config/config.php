507a823e77a22ad7651139f504a7d7efca76f97d
Jan Krepelka <jan.krepelka@gmail.com>
2014-10-05 20:43:24 +0200

    typo

---
+++ _prod/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('URL_SITE', '');
+define('URL_FULL', 'http://linguistica.rocks');
+define('URL_POSTS', 'content/posts');
+define('URL_DESIGN', 'content/design');
+define('URL_IMAGES', 'content/images');
+
+define('POSTS_LIMIT', 5);
+define('SEPARATOR',' | ');
+define('POSTS_LIMIT_RSS', 20);
+
+?>
